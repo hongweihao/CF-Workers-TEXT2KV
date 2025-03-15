8d48204057085dc5c31977e7c4cfccaec7e57d34
karl.hong <karlhong.weihao@qq.com>
2025-03-15 16:14:31 +0800

    update

--- _worker.js
+++ _worker.js
@@ -1,38 +1,36 @@
-let mytoken = 'passwd';
-
 export default {
     async fetch(request, env) {
         try {
-            mytoken = env.TOKEN || mytoken;
-
+            mytoken = env.TOKEN || 'passwd';
             if (!env.KV) {
                 throw new Error('KV 命名空间未绑定');
             }
 
-            console.log("request.url", request.url);
             const url = new URL(request.url);
-            const token = url.pathname === `/${mytoken}` ? mytoken : (url.searchParams.get('token') || "null");
+            const path = pathname.startsWith('/') ? pathname.substring(1) : pathname;
+            const parts = path.split('/');
 
-            console.log("url.pathname", url.pathname);
+            if (parts.length < 2) {
+                return createResponse('格式错误', 403);
+            }
 
+            // 第一部分是 token，第二部分是 fileName
+            const token = parts[0] || "null";
+            const fileName = parts[1];
             if (token !== mytoken) {
                 return createResponse('token 有误', 403);
             }
-
-            let fileName = url.pathname.startsWith('/') ? url.pathname.substring(1) : url.pathname;
-            fileName = fileName.toLowerCase(); // 将文件名转换为小写
-
-            switch (fileName) {
-                case "config":
-                case mytoken:
-                    return createResponse(configHTML(url.hostname, token), 200, { 'Content-Type': 'text/html; charset=UTF-8' });
-                case "config/update.bat":
-                    return createResponse(generateBatScript(url.hostname, token), 200, { "Content-Disposition": 'attachment; filename=update.bat', "Content-Type": "text/plain; charset=utf-8" });
-                case "config/update.sh":
-                    return createResponse(generateShScript(url.hostname, token), 200, { "Content-Disposition": 'attachment; filename=update.sh', "Content-Type": "text/plain; charset=utf-8" });
-                default:
-                    return await handleFileOperation(env.KV, fileName, url, token);
+            if (!fileName) {
+                return createResponse('File not found', 404);
             }
+
+            fileName = fileName.toLowerCase(); // 将文件名转换为小写
+            if(fileName === "config") {
+                // 返回管理页面
+                return createResponse(configHTML(url.hostname, token), 200, { 'Content-Type': 'text/html; charset=UTF-8' });
+            }
+            // 返回文件内容
+            return await handleFileOperation(env.KV, fileName, url, token);
         } catch (error) {
             console.error("Error:", error);
             return createResponse(`Error: ${error.message}`, 500);
@@ -48,28 +46,12 @@ export default {
  * @param {String} token - 认证 token
  */
 async function handleFileOperation(KV, fileName, url, token) {
-    const text = url.searchParams.get('text') || null;
-    const b64 = url.searchParams.get('b64') || null;
-
-    // 如果没有传递 text 或 b64 参数，尝试从 KV 存储中获取文件内容
-    if (!text && !b64) {
-        const value = await KV.get(fileName, { cacheTtl: 60 });
-        if (value === null) {
-            return createResponse('File not found', 404);
-        }
-        return createResponse(value);
+    // 获取文件内容    
+    const value = await KV.get(fileName, { cacheTtl: 60 });
+    if (value === null) {
+        return createResponse('File not found', 404);
     }
-
-    // 如果传递了 text 或 b64 参数，将内容写入 KV 存储
-    let content = text || base64Decode(replaceSpacesWithPlus(b64));
-    await KV.put(fileName, content);
-    const verifiedContent = await KV.get(fileName, { cacheTtl: 60 });
-
-    if (verifiedContent !== content) {
-        throw new Error('Content verification failed after write operation');
-    }
-
-    return createResponse(verifiedContent);
+    return createResponse(value);
 }
 
 /**
@@ -92,77 +74,6 @@ function createResponse(body, status = 200, additionalHeaders = {}) {
     return new Response(body, { status, headers });
 }
 
-/**
- * 解码 base64 字符串
- * @param {String} str - base64 字符串
- */
-function base64Decode(str) {
-    try {
-        const bytes = new Uint8Array(atob(str).split('').map(c => c.charCodeAt(0)));
-        return new TextDecoder('utf-8').decode(bytes);
-    } catch (error) {
-        throw new Error('Invalid base64 string');
-    }
-}
-
-/**
- * 将字符串中的空格替换为加号
- * @param {String} str - 输入字符串
- */
-function replaceSpacesWithPlus(str) {
-    return str.replace(/ /g, '+');
-}
-
-/**
- * 生成 Windows bat 脚本
- * @param {String} domain - 域名
- * @param {String} token - 认证 token
- */
-function generateBatScript(domain, token) {
-    return [
-        '@echo off',
-        'chcp 65001',
-        'setlocal',
-        '',
-        `set "DOMAIN=${domain}"`,
-        `set "TOKEN=${token}"`,
-        '',
-        'set "FILENAME=%~nx1"',
-        '',
-        'for /f "delims=" %%i in (\'powershell -command "$content = ((Get-Content -Path \'%cd%/%FILENAME%\' -Encoding UTF8) | Select-Object -First 65) -join [Environment]::NewLine; [convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($content))"\') do set "BASE64_TEXT=%%i"',
-        '',
-        'set "URL=https://%DOMAIN%/%FILENAME%?token=%TOKEN%^&b64=%BASE64_TEXT%"',
-        '',
-        'start %URL%',
-        'endlocal',
-        '',
-        'echo 更新数据完成,倒数5秒后自动关闭窗口...',
-        'timeout /t 5 >nul',
-        'exit'
-    ].join('\r\n');
-}
-
-/**
- * 生成 Linux sh 脚本
- * @param {String} domain - 域名
- * @param {String} token - 认证 token
- */
-function generateShScript(domain, token) {
-    return `#!/bin/bash
-export LANG=zh_CN.UTF-8
-DOMAIN="${domain}"
-TOKEN="${token}"
-if [ -n "$1" ]; then 
-  FILENAME="$1"
-else
-  echo "无文件名"
-  exit 1
-fi
-BASE64_TEXT=$(head -n 65 $FILENAME | base64 -w 0)
-curl -k "https://\${DOMAIN}/\${FILENAME}?token=\${TOKEN}&b64=\${BASE64_TEXT}"
-echo "更新数据完成"
-`;
-}
 
 /**
  * 生成 HTML 配置页面
@@ -275,16 +186,6 @@ function configHTML(domain, token) {
             <strong>服务域名:</strong> ${domain}<br>
             <strong>TOKEN:</strong> ${token}<br>
         </p>
-        <p class="tips"><strong>注意!</strong> 因URL长度内容所限，脚本更新方式一次最多更新65行内容</p>
-        <div class="flex-row">
-            <h2>Windows 脚本:</h2>
-            <button class="download-button" onclick="window.open('https://${domain}/config/update.bat?token=${token}&t=' + Date.now(), '_blank')">点击下载</button>
-        </div>
-            <pre><code>update.bat ip.txt</code></pre>
-        <div class="flex-row">
-            <h2>Linux 脚本:</h2>
-            <button class="download-button" onclick="copyLinuxScript()">点击复制</button>
-        </div>
             <pre><code class="language-bash">curl "https://${domain}/config/update.sh?token=${token}&t=$(date +%s%N)" -o update.sh && chmod +x update.sh</code></pre>
             <h2>在线文档查询:</h2>
         <div class="input-button-container">
